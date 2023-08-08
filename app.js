eacc6ba30f98511df5f25a7ffe99a2a26163bb33
Denis Kuzo <75919690+kidza20000@users.noreply.github.com>
2023-08-08 18:29:02 +0300

    .

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const authRoutes = require('./routes/auth')
+const analyticsRoutes = require('./routes/anatytics')
+const categoryRoutes = require('./routes/category')
+const orderRoutes = require('./routes/order')
+const positionRoutes = require('./routes/position')
+const keys = require('./config/keys')
+const app = express ()
+
+
+mongoose.connect('mongodb+srv://Denis:12345678910@cluster0.emtmsa4.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('MongoBD conected.'))
+    .catch(error => console.log(error))
+
+
+
+
+app.use(require('morgan')('dev'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(require('cors')())
+
+
+app.use('/api/auth',authRoutes )
+app.use('/api/analytics',analyticsRoutes )
+app.use('/api/category',categoryRoutes )
+app.use('/api/order',orderRoutes )
+app.use('/api/position',positionRoutes )
+
+
+
+module.exports = app
---
+++ routes/order.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const contoller = require('../controllers/order')
+const router = express.Router ()
+
+router.get('/',  contoller.getAll)
+router.post('/',  contoller.create)
+
+
+
+module.exports = router 
